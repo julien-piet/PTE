e284c06ad5632448788be0622a6396334b14a3e0
Aziz <abdulazizaaharbi@gmail.com>
2026-05-19 21:00:06 +0300

    Update shopping URL.

--- eval/program_html_evaluator.py
+++ eval/program_html_evaluator.py
@@ -23,7 +23,7 @@ from playwright.sync_api import Page
 DEFAULT_BASE_URLS: Dict[str, str] = {
     "__GITLAB__": "http://localhost:8023",
     "__REDDIT__": "http://localhost:9999",
-    "__SHOPPING__": "http://ec2-18-218-205-96.us-east-2.compute.amazonaws.com:8082",
+    "__SHOPPING__": "http://localhost:7770",
     "__SHOPPING_EXTRA__": "http://localhost:7790",
     "__SHOPPING_ADMIN__": "http://ec2-18-218-205-96.us-east-2.compute.amazonaws.com:8082/admin",
     "__MAP__": "http://localhost:3000",
